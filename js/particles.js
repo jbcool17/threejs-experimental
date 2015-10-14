e6f1ead9341f870c899ecb82282c80de5cb1861d
John Brilla <jbrilla17@gmail.com>
2015-10-14 16:57:01 +1100

    particle styles

--- js/particles.js
+++ js/particles.js
@@ -28,7 +28,7 @@ $(window).on('resize', function () {
 });
 
 // Set the background colour of the scene
-renderer.setClearColor(0x00aa33, 1);
+renderer.setClearColor(0x000000, 1);
 
 
 // LIGHT
@@ -37,6 +37,7 @@ var light = new THREE.PointLight(0xffffff);
 light.position.set(-100, 200, 100);
 
 scene.add(light);
+scene.fog = new THREE.FogExp2( 0x000000, 0.001 );
 
 
 //  $$$$$$\  $$$$$$$$\  $$$$$$\  $$\      $$\ $$$$$$$$\ $$$$$$$\ $$$$$$$$\ $$\     $$\ 
@@ -61,7 +62,7 @@ plane.position.y=-1
 plane.position.z=0
 
 // add the plane to the scene
-scene.add(plane);
+// scene.add(plane);
 
 
 // Add OrbitControls so that we can pan around with the mouse
@@ -76,15 +77,11 @@ var going = true;
 
 
 // create the particle variables
+var sprite = THREE.ImageUtils.loadTexture( "disc.png" );
 var particleCount = 1800,
     particles = new THREE.Geometry(),
-    pMaterial = new THREE.PointsMaterial({
-        color: 0xFFFFFF,
-        size: 20,
-        map: THREE.ImageUtils.loadTexture("particle.png"),
-        blending: THREE.AdditiveBlending,
-        transparent: true
-    });
+    pMaterial = new THREE.PointsMaterial({ size: 35, sizeAttenuation: false, map: sprite, alphaTest: 0.5, transparent: true });
+    pMaterial.color.setHSL( 1.0, 5, 0.7 );
 
 // particleSystem.sortParticles = true;
 
@@ -98,9 +95,13 @@ for (var p = 0; p < particleCount; p++) {
       pZ = Math.random() * 500 - 250,
       particle = new THREE.Vector3(pX, pY, pZ);
       
-
+    particle.velocity = new THREE.Vector3(
+      0,              // x
+      -Math.random(), // y: random vel
+      0);
   // add it to the geometry
   particles.vertices.push(particle);
+
 }
 
 // create the particle system
@@ -117,8 +118,51 @@ scene.add(particleSystem);
 //ANIMATE 
 //////////////////////////////
 var animate = function () {
+    var time = Date.now() * 0.00005;
+    // particleSystem.rotation.y += 0.01;
+    h = ( 360 * ( 1.0 + time ) % 360 ) / 360;
+    pMaterial.color.setHSL( h, 0.5, 0.5 );
 
-    particleSystem.rotation.y += 0.01;
+    for ( i = 0; i < scene.children.length; i ++ ) {
+
+        var object = scene.children[ i ];
+
+        var pCount = particleCount;
+
+        if ( object instanceof THREE.Points ) {
+
+            object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
+            // while (pCount--) {
+
+            //     // get the particle
+            //     var particle =
+            //       particles.vertices[pCount];
+
+            //     // check if we need to reset
+            //     if (object.position.y < -200) {
+            //       object.position.y = 200;
+            //       object.position.y = 0;
+            //     }
+
+            //     // update the velocity with
+            //     // a splat of randomniz
+            //     object.position.y -= Math.random() * .1;
+
+            //     // and the position
+            //     object.position.add(
+            //       object.position);
+            //   }
+
+            //   // flag to the particle system
+            //   // that we've changed its vertices.
+            //   particleSystem.
+            //     geometry.
+            //     __dirtyVertices = true;
+
+        }
+
+    }
+
 
     requestAnimationFrame(animate);
     
